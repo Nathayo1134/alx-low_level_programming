fd76e7d620ba13affd47c3d6dac995aca7a8a5ff
Balogun <balogunayomide85@gmail.com>
2023-02-22 16:56:09 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -5,7 +5,7 @@
  * @c: the character to print 
  *
  * Return: On success 1.
- * O error, -1 is returned, and errno is set appropriately.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
